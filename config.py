131edfa01c2ad98fd76bedf56c56fc547ad5a979
Henrik Grankvist <hg@conscia.com>
2020-01-10 14:38:55 +0100

    more changes

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
 
-# asd
+basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'bf8c5e12b5d0eb551b76003b98c7731889a91451a071ddad7a6b33bb8f887268'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///" + os.path.join(basedir, "app.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
