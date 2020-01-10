44553ffe94e5e85796f8bf260c1f668d093d9f8b
Henrik Grankvist <hg@conscia.com>
2020-01-10 14:38:44 +0100

    lots of changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from config import Config
 import os
+from flask_sqlalchemy import SQLAlchemy # pip install flask-sqlalchemy
+from flask_login import LoginManager # pip install flask-login
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+login = LoginManager(app)
+login.login_view = "login" # if a page requires login to be viewed, redirect them to the login page
 
-from app import routes
+
+
+from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
 from flask import render_template, request, url_for, redirect, flash
-from app import app
+from app import app, db
 from app.forms import LoginForm, RegisterForm
+from app.models import User
+from flask_login import current_user, login_user, login_required, logout_user
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
@@ -9,21 +12,65 @@ def index():
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
+
+    # if the user is already logged in. Redirect them away
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+
     form = LoginForm()
 
     if form.validate_on_submit(): # validate_on_submit is needed to validate the output according to forms.py
-        user = form.username.data
-        password = form.password.data
-        flash(user + ' Inloggningen misslyckades')
-        return redirect(url_for("login"))
+        user = User.query.filter_by(username=form.username.data).first()
+
+        # user is None if it was not found in the database
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password")
+            return redirect(url_for("login"))
+
+        login_user(user, remember=form.remember_me.data)
+
+        next_page = request.args.get("next")
+
+        if not next_page or url_parse(next_page).netloc != "":
+            next_page = url_for("index")
+
+        # Redirect the user to the page they got from.
+        return redirect(next_page)
 
     return render_template("login.html", title='Logga in', form=form)
 
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
+
 @app.route('/register', methods=["GET", "POST"])
 def register():
+
+    # if the user is already logged in. Redirect them away
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+
     form = RegisterForm()
+
+    if form.validate_on_submit():
+        user = User(username=form.username.data)
+
+        # Check if user is already registered
+        if User.query.filter_by(username=form.username.data).first() is not None:
+            flash("Epost-adressen är redan registerad.")
+            return redirect(url_for("register"))
+
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        flash('Grattis, du är nu en registerad medlem!')
+        return redirect(url_for('login'))
+
     return render_template("register.html", title='Registera ny medlem', form=form)
 
 @app.route('/page')
+@login_required
 def page():
     return render_template("page.html", title='Test page')
